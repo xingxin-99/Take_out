b9047fb56407b6a9027ab5e004d52932eae6b71a
xingxin-99 <755574505@qq.com>
2023-06-04 11:02:34 +0800

    使用SpringCache缓存套餐数据

--- src/main/java/com/star/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/star/reggie/filter/LoginCheckFilter.java
@@ -30,8 +30,11 @@ public class LoginCheckFilter implements Filter {
                 "/backend/**",
                 "/front/**",
                 "/user/sendMsg",
-                "/user/login"
-
+                "/user/login",
+                "/doc.html",
+                "webjars/**",
+                "/swagger-resources",
+                "/v2/api-docs"
         };
 
         boolean check = matchUrl(urls,request.getRequestURI());
